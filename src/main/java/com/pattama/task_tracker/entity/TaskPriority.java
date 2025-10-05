fe50c58847dca005ded803e63878a7c1d791ee83
Pattamaporn <pttamaporn.k@gmail.com>
2025-10-05 23:24:46 +0700

    feat(entity): add remaining entities

---
+++ src/main/java/com/pattama/task_tracker/entity/TaskPriority.java
@@ -0,0 +1,9 @@
+package com.pattama.task_tracker.entity;
+
+public enum TaskPriority {
+
+    LOW,
+    MEDIUM,
+    HIGH
+
+}
---
+++ src/main/java/com/pattama/task_tracker/entity/TaskStatus.java
@@ -0,0 +1,9 @@
+package com.pattama.task_tracker.entity;
+
+public enum TaskStatus {
+
+    TODO,
+    IN_PROGRESS,
+    COMPLETED
+
+}
